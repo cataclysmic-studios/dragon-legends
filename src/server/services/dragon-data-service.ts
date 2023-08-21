74aea849a96c97d3ce68393a9284078ec33919fd
AlphaRunic <roleyspersonal@gmail.com>
2023-08-21 13:55:15 -0400

    fix: DragonDataService#remove() not removing dragon ID from respective habitat

--- src/server/services/dragon-data-service.ts
+++ src/server/services/dragon-data-service.ts
@@ -5,6 +5,7 @@ import { BuildingDataService } from "./building-data-service";
 import { Dragon } from "shared/data-models/dragons";
 import { Habitat } from "shared/data-models/habitats";
 import { Events, Functions } from "server/network";
+import { Exception, MissingDataException } from "shared/exceptions";
 
 const { addDragonXP } = Events;
 const { getDragonData } = Functions;
@@ -44,11 +45,20 @@ export class DragonDataService implements OnInit {
   }
 
   public remove(player: Player, dragonID: string): void {
+    const dragon = this.get(player, dragonID);
+    if (!dragon)
+      throw new MissingDataException(dragonID, "Attempt to remove non-existent dragon data");
+
+    const habitat = this.buildingData.get<Habitat>(player, dragon.habitatID)!;
     const newDragons = this.data
       .get<Dragon[]>(player, "dragons")
       .filter(d => d.id !== dragonID);
 
+    habitat.dragonIDs = habitat.dragonIDs
+      .filter(id => id !== dragonID);
+
     this.data.set(player, "dragons", newDragons);
+    this.buildingData.update(player, habitat);
   }
 
   public get(player: Player, dragonID: string): Maybe<Dragon> {
