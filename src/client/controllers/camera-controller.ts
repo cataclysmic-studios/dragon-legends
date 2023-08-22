b04e8e89124fe251304ea091bb6500a259f3d1db
AlphaRunic <roleyspersonal@gmail.com>
2023-08-21 20:30:28 -0400

    fix: disallow camera panning if the current screen isn't main

--- src/client/controllers/camera-controller.ts
+++ src/client/controllers/camera-controller.ts
@@ -1,7 +1,9 @@
 import { Controller, OnInit, OnRender } from "@flamework/core";
 import { StarterGui, Workspace as World } from "@rbxts/services";
-import { BuildingPlacementController } from "./building-placement-controller";
+
+import { UIController } from "./ui-controller";
 import { MouseController } from "./mouse-controller";
+import { BuildingPlacementController } from "./building-placement-controller";
 
 @Controller()
 export class CameraController implements OnInit, OnRender {
@@ -12,8 +14,9 @@ export class CameraController implements OnInit, OnRender {
   private zoomPosition = 2;
 
   public constructor(
-    private readonly building: BuildingPlacementController,
-    private readonly mouse: MouseController
+    private readonly ui: UIController,
+    private readonly mouse: MouseController,
+    private readonly building: BuildingPlacementController
   ) { }
 
   public onInit(): void {
@@ -24,6 +27,7 @@ export class CameraController implements OnInit, OnRender {
     const islandPosition = World.Islands.Main.Position;
     this.cameraPart.Position = islandPosition.add(new Vector3(0, 15 - islandPosition.Y, 0));
     this.mouse.onScroll(direction => {
+      if (this.ui.current !== "Main") return;
       if (this.zoomPosition >= 8 && direction < 0) return;
       if (this.zoomPosition <= -15 && direction > 0) return;
       this.zoomDirection -= direction;
@@ -32,6 +36,8 @@ export class CameraController implements OnInit, OnRender {
   }
 
   public onRender(dt: number): void {
+    if (this.ui.current !== "Main") return;
+
     const { camera, cameraPart, bounds } = this;
     const { X: dx, Y: dy } = this.mouse.delta().div(16);
     const movementCorrection = CFrame.Angles(-math.rad(cameraPart.Orientation.X), 0, 0);
@@ -52,8 +58,8 @@ export class CameraController implements OnInit, OnRender {
     if (z <= bounds.Position.Z - bounds.Size.Z / 2)
       z = -bounds.Size.Z / 2;
 
-    const mouseDown = this.mouse.down && !this.building.isDragging();
-    const mouseBehavior = mouseDown ? Enum.MouseBehavior.LockCurrentPosition : Enum.MouseBehavior.Default;
+    const notDragging = this.mouse.down && !this.building.isDragging();
+    const mouseBehavior = notDragging ? Enum.MouseBehavior.LockCurrentPosition : Enum.MouseBehavior.Default;
     this.mouse.setBehavior(mouseBehavior);
     cameraPart.Position = new Vector3(x, movedPosition.Y, z)
       .add(cameraPart.CFrame.LookVector.mul(this.zoomDirection * 60 * dt));
